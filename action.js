7022e1afb68c1869897732beacc0800dc665b6c2
Sarina-Stoker <sstoker1@hawk.iit.edu>
2018-02-18 19:25:12 -0600

    Added age checker code to submit function.

--- action.js
+++ action.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
        
         var dobValue=document.getElementById("dob").value;
         var dob= new Date(dobValue);
         
-        var month=dob.getMonth();/*substr(0,2);*/
-        var day=dob.getYear();/*substr(3,2);*/
-        var year=dob.getYear();/*substr(6,4);*/
+        var month=dob.getMonth();
+        var day=dob.getYear();
+        var year=dob.getYear();
         
         var today = new Date();
 
@@ -16,20 +16,36 @@ $(document).ready(function () {
         else if(today.getMonth()==month && today.getDay()/1<day){
           age--;     
         }
-       /* if (age < 18) {
+        if (age < 18) {
           alert("You have to be 18 or older to sign up. Sorry!");
-        } */
+        } 
      
-  });
+  }); */
     
 
 
 $('#Contact').on('submit',
   function(e) {
+    var dobValue=document.getElementById("dob").value;
+    var dob= new Date(dobValue);
+        
+    var month=dob.getMonth();/*substr(0,2);*/
+    var day=dob.getYear();/*substr(3,2);*/
+    var year=dob.getYear();/*substr(6,4);*/
+        
+    var today = new Date();
+
+    var age = today.getYear() - year;
+    if(today.getMonth() < month){
+         age--;     
+    }
+    else if(today.getMonth()==month && today.getDay()/1<day){
+      age--;  
+    }    
     
     if(age < 18){
-          alert("You have to be 18 or older to sign up. Sorry!");
-        }
+      alert("You have to be 18 or older to sign up. Sorry!");
+    }
     else{
     console.log('The form was submitted.');
      e.preventDefault();
