8eea5386fc962f547d22644073b94a2e7e303c39
Sarina-Stoker <sstoker1@hawk.iit.edu>
2018-02-18 23:25:05 -0600

    Changed file name from design to site.js

---
+++ site.js
@@ -0,0 +1,31 @@
+
+   $('#contact').on('submit',
+  function(e) {
+    var dobValue=document.getElementById("dob").value;
+    var dob= new Date(dobValue);
+        
+    var month=dob.getMonth();/*substr(0,2);*/
+    var day=dob.getYear();/*substr(3,2);*/
+    var year=dob.getYear();/*substr(6,4);*/
+        
+    var today = new Date();
+
+    var age = today.getYear() - year;
+    if(today.getMonth() < month){
+         age--;     
+    }
+    else if(today.getMonth()==month && today.getDay()/1<day){
+      age--;  
+    }    
+    
+    if(age < 18){
+      alert("You have to be 18 or older to sign up. Sorry!");
+    }
+    else{
+    console.log('The form was submitted.');
+    alert("Thank you! Your form was submitted.");
+     e.preventDefault();
+    }
+    
+  }
+ );
